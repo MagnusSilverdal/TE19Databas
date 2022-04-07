41cb7400507354806004ac22593b16f8b0eade28
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2022-04-07 11:49:45 +0200

    Added some code to start with

---
+++ src/Controller.java
@@ -0,0 +1,19 @@
+/**
+ * This is a class
+ * Created 2022-04-07
+ *
+ * @author Magnus Silverdal
+ */
+public class Controller {
+    // Views and models
+
+    // Constructor and methods
+    public Controller() {
+
+    }
+
+    // Main
+    public static void main(String[] args) {
+        Controller c = new Controller();
+    }
+}
